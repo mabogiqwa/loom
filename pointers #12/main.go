a275d2d61f133a070b6fdca3e221ddeaecac788b
mabogiqwa <mabogiqwa1@gmail.com>
2026-02-08 23:43:27 +0200

    new commit

--- pointers #12/main.go
+++ pointers #12/main.go
@@ -5,6 +5,20 @@ import "fmt"
 //non-pointer objects: strings, ints, floats, booleans, arrays, structs
 //pointer-wrapper vals: slices, maps, functions
 
+func changeValue(x string) {
+	x = "x^3dxnc$3#"
+}
+
 func main() {
-	fmt.Println()
+	name := "flint"
+
+	changeValue(name)
+
+	fmt.Println("Memory address of the variable name is: ", &name)
+	m := &name //variables m and name share the same memory addresses
+	fmt.Println(name)
+	fmt.Println(*m)
+
+	*m = "stone"
+	fmt.Println(name)
 }
