9bd69260cfef9465567dffbc6996e7a251f1ab9a
mabogiqwa <mabogiqwa1@gmail.com>
2026-02-08 23:15:30 +0200

    pointers

---
+++ pointers #12/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+//non-pointer objects: strings, ints, floats, booleans, arrays, structs
+//pointer-wrapper vals: slices, maps, functions
+
+func main() {
+	fmt.Println()
+}
