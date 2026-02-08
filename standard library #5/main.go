6c45e78dd2809243dd10b4029e79c15b61302fcf
mabogiqwa <mabogiqwa1@gmail.com>
2026-02-08 16:53:50 +0200

    standard library

--- standard library #5/main.go
+++ standard library #5/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sort"
 	"strings"
 )
 
@@ -13,5 +14,22 @@ func main() {
 	fmt.Println(sen)
 	fmt.Println(sen2)
 
-	fmt.Println(strings.Contains(sen, "hello"))
+	fmt.Println(strings.Contains(sen, "Hello"))         //Checks if the substring "Hello" is in the variable sen
+	fmt.Println(strings.ReplaceAll(sen, "Hello", "Hi")) //Replaces all instances of the substring "Hello" with "Hi"
+	fmt.Println(strings.ToUpper(sen))                   //Transforms all characters to upper case
+	fmt.Println(strings.Split(sen, " "))                //splits words in a string into an array
+	ages := []int{45, 66, 12, 45, 8, 63, 12, 90, 71}
+
+	sort.Ints(ages) //sorts array in ascending
+	fmt.Println(ages)
+
+	index := sort.SearchInts(ages, 8)
+	fmt.Println(index)
+
+	variousNames := []string{"Mabo", "Yamamoto", "Luigi", "Goku", "Yuji"}
+
+	sort.Strings(variousNames) //Sorts according to alphabetical order
+	fmt.Println(variousNames)
+
+	//fmt.Println("Original string:", sen)
 }
