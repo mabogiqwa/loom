dfc4b17b9cf9b539e9ce7e62fada04f365818f4d
mabogiqwa <mabogiqwa1@gmail.com>
2026-02-08 17:10:50 +0200

    loops

--- loops #6/main.go
+++ loops #6/main.go
@@ -3,9 +3,27 @@ package main
 import "fmt"
 
 func main() {
-	x := 0
-	for x < 5 {
-		fmt.Println("Value of x is:", x)
-		x++
+	/*
+		x := 0
+		for x < 5 {
+			fmt.Println("Value of x is:", x)
+			x++
+		}
+	*/
+	//Alternative definition
+	for i := 0; i < 5; i++ {
+		fmt.Print("Value of i is: ", i, "| ")
+	}
+	fmt.Println()
+
+	names := []string{"Mabo", "Yamamoto", "Luigi", "Goku", "Yuji"}
+	/*
+		for i := 0; i < len(names); i++ {
+			fmt.Println(names[i])
+		}
+	*/
+
+	for index, value := range names {
+		fmt.Printf("The position at index %v is %v\n", index, value)
 	}
 }
